289e5b01754a4034bec92fcc471f074d74b55030
Dhairy Srivastava <dhairysrivastava5@gmail.com>
2025-06-17 18:23:40 +0530

    Initial commit

---
+++ CatCare/CatCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatCareApp.swift
+//  CatCare
+//
+//  Created by dhairy on 17/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CatCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatCare/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CatCare
+//
+//  Created by dhairy on 17/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Cat Care App")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()	}
